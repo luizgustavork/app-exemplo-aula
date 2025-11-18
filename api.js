78ebd005fdea5e7e60ccfea33f27379a6bc439ab
Luiz Henrique Custódio Mendes Marques <luiz@MacBook-Air-de-Luiz.local>
2025-11-17 21:40:56 -0300

    aula 3

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { sequelize } from './models/index.js';
+import { Especialidades, sequelize } from './models/index.js';
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +8,51 @@ app.use(express.json());
 
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'Ok' });
+});
+
+app.post('/especialidades', async (req, res) => {
+  try{
+    const especialidade = await Especialidades.create(req.body);
+    res.status(201).json(especialidade);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.get('/especialidades', async (req, res) => {
+  try {
+    const especialidades = await Especialidades.findAll();
+    res.status(200).json(especialidades);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/especialidades/:id', async (req, res) => {
+  try {
+    const especialidade = await Especialidades.findByPk(req.params.id);
+    if (especialidade) {
+      res.status(200).json(especialidade);
+    } else {
+      res.status(404).json({ error: 'Especialidade não encontrada' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.delete('/especialidades/:id', async (req, res) => {
+  try {
+    const especialidade = await Especialidades.findByPk(req.params.id);
+    if (especialidade) {
+      await especialidade.destroy();
+       return res.status(200).json({ message: 'Especialidade deletada com sucesso' });
+    } else {
+      res.status(404).json({ error: 'Especialidade não encontrada' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // Sincronizar banco de dados e iniciar servidor
